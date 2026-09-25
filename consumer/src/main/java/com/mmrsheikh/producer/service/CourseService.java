a618830deb2c735ef453879b8e4be5d9f9780e6e
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-26 03:47:05 +0600

    add

--- consumer/src/main/java/com/mmrsheikh/producer/service/CourseService.java
+++ consumer/src/main/java/com/mmrsheikh/producer/service/CourseService.java
@@ -1,19 +1,16 @@
 package com.mmrsheikh.producer.service;
 
 import com.mmrsheikh.producer.models.Course;
-import com.mmrsheikh.producer.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class CourseService {
 
-    private final CourseRepository courseRepository;
+
 
     private final KafkaTemplate<String, Course> kafkaTemplate;
 
@@ -31,40 +28,6 @@ public class CourseService {
 
     public String getMessage() {
         return message;
-    }
-
-    public Course save(Course course) {
-        Course saved = courseRepository.save(course);
-
-        return saved;
-    }
-
-    public List<Course> getAll() {
-        return courseRepository.findAll();
-    }
-
-
-    public Course getById(Long id) {
-        Course found = courseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No Course found"));
-
-        return found;
-    }
-
-    public Course update(Long id, Course course) {
-        Course found = courseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No Course found"));
-
-        course.setCourseId(found.getCourseId());
-        return courseRepository.save(course);
-
-    }
-
-    public void delete(Long id) {
-        Course found = courseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No Course found"));
-        courseRepository.delete(found);
-
     }
 
 
--- producer/src/main/java/com/mmrsheikh/producer/service/CourseService.java
+++ producer/src/main/java/com/mmrsheikh/producer/service/CourseService.java
@@ -1,18 +1,17 @@
 package com.mmrsheikh.producer.service;
 
 import com.mmrsheikh.producer.models.Course;
-import com.mmrsheikh.producer.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
 
-    private final CourseRepository courseRepository;
+
 
     private final KafkaTemplate<String, Course> kafkaTemplate;
 
@@ -23,39 +22,6 @@ public class CourseService {
 
     }
 
-    public Course save(Course course) {
-        Course saved = courseRepository.save(course);
-
-        return saved;
-    }
-
-    public List<Course> getAll() {
-        return courseRepository.findAll();
-    }
-
-
-    public Course getById(Long id) {
-        Course found = courseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No Course found"));
-
-        return found;
-    }
-
-    public Course update(Long id, Course course) {
-        Course found = courseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No Course found"));
-
-        course.setCourseId(found.getCourseId());
-        return courseRepository.save(course);
-
-    }
-
-    public void delete(Long id) {
-        Course found = courseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No Course found"));
-        courseRepository.delete(found);
-
-    }
 
 
 }
